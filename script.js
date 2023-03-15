a7b8f5801e3d011f90c3609f11baae69b8a7b813
Thiago Marsili <thihoma@gmail.com>
2023-03-15 20:50:41 -0300

    PreEntrega1

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-// Pide al usuario que ingrese dos números//
-let num1 = parseFloat(prompt("Ingrese el primer número:"));
-let num2 = parseFloat(prompt("Ingrese el segundo número:"));
-
-// Pide al usuario que ingrese una operación//
-let operator = prompt("Ingrese la operación (+, -, *, /):");
-
-// Evalua la operación y realiza el cálculo //
-if (operator === "+") {
-  var result = num1 + num2;
-} else if (operator === "-") {
-  var result = num1 - num2;
-} else if (operator === "*") {
-  var result = num1 * num2;
-} else if (operator === "/") {
-  var result = num1 / num2;
-} else {
-  var result = "Operación no válida";
+while (true) {
+  // Pide al usuario que ingrese dos números//
+  let num1 = parseFloat(prompt("Ingrese el primer número:"));
+  let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+  
+  // Pide al usuario que ingrese una operación//
+  let operator = prompt("Ingrese la operación (+, -, *, /):");
+  
+  // Evalua la operación y realiza el cálculo //
+  if (operator === "+") {
+    var result = num1 + num2;
+  } else if (operator === "-") {
+    var result = num1 - num2;
+  } else if (operator === "*") {
+    var result = num1 * num2;
+  } else if (operator === "/") {
+    var result = num1 / num2;
+  } else {
+    var result = "Operación no válida";
+  }
+  
+  // Mostramos el resultado al usuario//
+  alert("El resultado es: " + result);
 }
-
-// Mostramos el resultado al usuario//
-alert("El resultado es: " + result);
